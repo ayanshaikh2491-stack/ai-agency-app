724f78df422d780541d0ee00da97921b3efa9e57
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-02 12:32:35 +0530

    Create page.tsx

---
+++ app/pricing/page.tsx
@@ -0,0 +1,22 @@
+export default function Pricing() {
+  return (
+    <>
+      <h1>Pricing Plans</h1>
+
+      <div className="card">
+        <h3>Starter</h3>
+        <p>₹5,000 / month</p>
+      </div>
+
+      <div className="card">
+        <h3>Pro</h3>
+        <p>₹15,000 / month</p>
+      </div>
+
+      <div className="card">
+        <h3>Enterprise</h3>
+        <p>Custom pricing</p>
+      </div>
+    </>
+  );
+}
