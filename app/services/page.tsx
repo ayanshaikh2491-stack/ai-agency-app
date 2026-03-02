7bcfd867ac8d37f6d69debdf6170bf909ebc409d
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-02 17:28:20 +0530

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,22 +1,5 @@
-export default function Services() {
-  return (
-    <>
-      <h1>Our Services</h1>
+import ServicesSection from "@/components/ui/ServicesSection";
 
-      <div className="card">
-        <h3>AI Chatbots</h3>
-        <p>Smart chat systems for customer support and automation.</p>
-      </div>
-
-      <div className="card">
-        <h3>Workflow Automation</h3>
-        <p>Automate repetitive tasks and increase productivity.</p>
-      </div>
-
-      <div className="card">
-        <h3>Marketing AI Tools</h3>
-        <p>Data-driven marketing automation solutions.</p>
-      </div>
-    </>
-  );
+export default function ServicesPage() {
+  return <ServicesSection />;
 }
