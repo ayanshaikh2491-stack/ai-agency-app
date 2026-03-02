1716f89ff9b364be8a1dde4c571a7d071e7d7965
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-02 17:18:38 +0530

    Create Sidebar.tsx

---
+++ components/ui/dashboard/Sidebar.tsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+  return (
+    <div className="w-64 bg-slate-900 p-6 border-r border-slate-800">
+      <h2 className="text-xl font-bold">
+        Admin Panel
+      </h2>
+    </div>
+  );
+}
