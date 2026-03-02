80d8546b09d02336fe36ce561c2ddf42c49e2816
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-02 12:33:14 +0530

    Create page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <>
+      <h1>Contact Us</h1>
+
+      <div className="card">
+        <p>Email: contact@aiagency.com</p>
+        <p>Phone: +91 9876543210</p>
+      </div>
+    </>
+  );
+}
