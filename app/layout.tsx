3cd1eaf4525b776b4f2c5fdaffb2da7b3dd1a130
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-02 18:18:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "@/components/ui/Navbar";
+import Footer from "@/components/ui/Footer";
 
 export const metadata = {
   title: "Organic Growth Agency",
