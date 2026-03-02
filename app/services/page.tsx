8448ecd5b2259223420916c0f04384863b64f91f
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-02 12:31:50 +0530

    Create page.tsx

---
+++ app/services/page.tsx
@@ -0,0 +1,22 @@
+export default function Services() {
+  return (
+    <>
+      <h1>Our Services</h1>
+
+      <div className="card">
+        <h3>AI Chatbots</h3>
+        <p>Smart chat systems for customer support and automation.</p>
+      </div>
+
+      <div className="card">
+        <h3>Workflow Automation</h3>
+        <p>Automate repetitive tasks and increase productivity.</p>
+      </div>
+
+      <div className="card">
+        <h3>Marketing AI Tools</h3>
+        <p>Data-driven marketing automation solutions.</p>
+      </div>
+    </>
+  );
+}
