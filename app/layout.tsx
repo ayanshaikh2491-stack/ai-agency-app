670d8925d2732a18debdc22841be9f6b8f93ca19
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-03 11:20:49 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
-import "./globals.css"
-
-export const metadata = {
-  title: "Organic Growth Agency",
-  description: "We grow brands organically through content and strategy.",
-}
+import Navbar from "@/components/ui/Navbar";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className="bg-slate-950 text-white">
+      <body className="bg-black text-white">
+        <Navbar />
         {children}
       </body>
     </html>
-  )
+  );
 }
