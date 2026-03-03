ceca82703de61b8c8b48254a04fe276f14ac4e8b
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-03 13:08:53 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import PricingSection from "../components/ui/PricingSection";
 
 export default function HomePage() {
   return (
-    <>
+    <main className="bg-black text-white">
       <Hero />
       <ServicesSection />
       <PricingSection />
-    </>
+    </main>
   );
 }
