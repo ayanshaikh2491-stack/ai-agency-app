c14708725c4b0b96c5a486d472689058ae2ee046
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-03 13:08:28 +0530

    Update page.tsx

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,8 +2,8 @@ import PricingSection from "../../components/ui/PricingSection";
 
 export default function PricingPage() {
   return (
-    <div className="min-h-screen bg-black text-white pt-20">
+    <main className="min-h-screen bg-black text-white pt-20">
       <PricingSection />
-    </div>
+    </main>
   );
 }
