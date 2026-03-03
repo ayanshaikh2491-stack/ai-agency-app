dc8eb575eaf6d10d2e1aeefc88cbdc2cd6a333d7
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-04 01:24:16 +0530

    Delete components/ui/ChatFloating.tsx

--- components/ui/ChatFloating.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function ChatFloating() {
-  return (
-    <button className="fixed bottom-6 right-6 bg-purple-600 text-white w-14 h-14 rounded-full shadow-lg hover:bg-purple-700 transition">
-      💬
-    </button>
-  );
-}
