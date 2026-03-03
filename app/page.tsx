104be5cf115899cfb3c548034445e7c0cf9a3633
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-03 12:57:07 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Hero from "@/components/ui/Hero";
-import ServicesSection from "@/components/ui/ServicesSection";
-import PricingSection from "@/components/ui/PricingSection";
+import Hero from "../components/ui/Hero";
+import ServicesSection from "../components/ui/ServicesSection";
+import PricingSection from "../components/ui/PricingSection";
 
 export default function HomePage() {
   return (
