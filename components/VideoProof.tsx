b3831cfa3a509288f0e5b63abfe4c0c2dc3c548d
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-04 22:30:56 +0530

    Create VideoProof.tsx

---
+++ components/VideoProof.tsx
@@ -0,0 +1,19 @@
+export default function VideoProof(){
+
+  return(
+
+    <section className="max-w-6xl mx-auto py-24 px-6 text-center">
+
+      <h2 className="text-4xl font-bold mb-10">
+        Real Client Results
+      </h2>
+
+      <div className="bg-zinc-900 h-80 rounded-xl flex items-center justify-center">
+        Video Proof
+      </div>
+
+    </section>
+
+  )
+
+}
