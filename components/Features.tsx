7ac6cfcccf4b7259d8eedb52a5b0a76ad0562f6f
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-04 22:30:11 +0530

    Create Features.tsx

---
+++ components/Features.tsx
@@ -0,0 +1,31 @@
+export default function Features(){
+
+  return(
+
+    <section className="max-w-6xl mx-auto py-24 px-6">
+
+      <h2 className="text-4xl font-bold mb-12">
+        Powerful AI Features
+      </h2>
+
+      <div className="grid grid-cols-3 gap-8">
+
+        <div className="bg-zinc-900 p-6 rounded-xl">
+          AI Marketing Teams
+        </div>
+
+        <div className="bg-zinc-900 p-6 rounded-xl">
+          Automated Workflows
+        </div>
+
+        <div className="bg-zinc-900 p-6 rounded-xl">
+          Growth Analytics
+        </div>
+
+      </div>
+
+    </section>
+
+  )
+
+}
