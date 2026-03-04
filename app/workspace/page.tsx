b745b00a231c570c6c5df53d9265eb131f7412b6
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-04 17:05:09 +0530

    Update page.tsx

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import ManagerEngine from "../../components/ManagerEngine";
+import ManagerEngine from "@/components/ManagerEngine";
 
 export default function Workspace() {
+
   return (
     <div className="min-h-screen bg-black text-white p-6">
+
       <ManagerEngine />
+
     </div>
   );
 }
