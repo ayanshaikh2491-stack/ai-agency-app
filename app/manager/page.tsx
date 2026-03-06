a9b8689e6d6e113b74d0fdf6b6b748f7d37375fa
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-07 00:56:01 +0530

    Create page.tsx

---
+++ app/manager/page.tsx
@@ -0,0 +1,15 @@
+import ManagerEngine from "@/components/ManagerEngine"
+
+export default function ManagerPage(){
+
+return(
+
+<main className="bg-black text-white min-h-screen p-10">
+
+<ManagerEngine />
+
+</main>
+
+)
+
+}
