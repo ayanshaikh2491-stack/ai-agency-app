676d85b1378863ceddfc0072c98c7c453024fe77
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-08 01:32:59 +0530

    Create agentExecutor.ts

---
+++ lib/agentExecutor.ts
@@ -0,0 +1,54 @@
+export const agents = {
+
+designer:{
+name:"Isha Verma",
+role:"UI Designer",
+model:"llama-3.3-70b-versatile",
+prompt:`
+You are a senior UI designer.
+Design modern website layouts and user interfaces.
+Focus on clean UX and conversion focused design.
+`
+},
+
+frontend:{
+name:"Aarav Kapoor",
+role:"Frontend Developer",
+model:"llama-3.3-70b-versatile",
+prompt:`
+You are a senior React + Tailwind developer.
+Build responsive UI components and page structure.
+`
+},
+
+backend:{
+name:"Kabir Mehta",
+role:"Backend Developer",
+model:"llama-3.3-70b-versatile",
+prompt:`
+You are a backend engineer.
+Design APIs, database structure and server logic.
+`
+},
+
+seo:{
+name:"Rohan Sharma",
+role:"SEO Specialist",
+model:"llama-3.3-70b-versatile",
+prompt:`
+You are an SEO expert.
+Generate keyword strategy and ranking plan.
+`
+},
+
+marketing:{
+name:"Riya Gupta",
+role:"Marketing Strategist",
+model:"llama-3.3-70b-versatile",
+prompt:`
+You are a marketing strategist.
+Create marketing campaigns and funnels.
+`
+}
+
+}
